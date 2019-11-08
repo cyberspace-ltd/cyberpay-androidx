3e9bddf52efd1d77972bab55d6b6375ce136d6f1
Shaba Okare Michael <shabaokare@gmail.com>
2019-11-08 12:50:45 +0100

    Transaction Service network

--- cyberpaysdk/src/main/java/com/cyberspace/cyberpaysdk/data/transaction/service/TransactionService.kt
+++ cyberpaysdk/src/main/java/com/cyberspace/cyberpaysdk/data/transaction/service/TransactionService.kt
@@ -3,29 +3,39 @@ package com.cyberspace.cyberpaysdk.data.transaction.service
 import com.google.gson.JsonObject
 import io.reactivex.Observable
 import okhttp3.RequestBody
-import retrofit2.http.Body
-import retrofit2.http.POST
+import org.json.JSONObject
+import retrofit2.http.*
 
 interface TransactionService {
 
     @POST("payments")
     fun beginTransaction(@Body transactionService : Map<String, String>) : Observable<JsonObject>
 
-    /*
-
-    @POST("payments")
-    Call<ApiResponse<TransactionResponse>> beginTransaction(@Body RequestBody params);
-
     @POST("payments/card")
-    Call<ApiResponse<ChargeResponse>> chargeCard(@Body RequestBody params);
-
+    fun chargeCard(@Body transactionService: Map<String, String>) : Observable<JsonObject>
 
     @GET("payments/{transactionReference}")
-    Call<ApiResponse<VerifyTransactionResponse>> verifyTransaction(@Path("transactionReference") String transactionReference);
-
+    fun verifyTransaction(@Path("transactionReference") transactionReference : String) : Observable<JsonObject>
 
     @GET("transactions/transactionsBymerchantRef")
-    Call<ApiResponse<MerchantTransactionResponse>> verifyMerchantTransaction(@Query("merchantRef") String merchantRef);
+    fun verifyMerchantTransaction(@Query("merchantRef") merchantRef : String) : Observable<JsonObject>
+
+    @POST("payments/otp")
+    fun verifyOtp(@Body transactionService : Map<String , String>) : Observable<JsonObject>
+
+    @POST("payments/bank/otp/{value}")
+    fun verifyBankOtp(@Body transactionService: Map<String, String>, @Path("value") value : String) : Observable<JsonObject>
+
+    @POST("payments/bank")
+    fun chargeBank(@Body transactionService: Map<String, String>) : Observable<JsonObject>
+
+    @POST("payments/bank/enrol/otp")
+    fun enrolOtp(@Body transactionService: Map<String, String>) : Observable<JsonObject>
+
+    @GET("banks")
+    fun getBank() : Observable<JsonObject>
+
+    /*
 
 
     @POST("payments/otp")
