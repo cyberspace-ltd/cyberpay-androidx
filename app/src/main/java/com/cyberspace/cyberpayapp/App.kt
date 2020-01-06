9f1a3d20560e43a009251d4c26ffbb71e1a9f605
sunday-onoriode <topracer1988@gmail.com>
2020-01-06 10:23:25 +0100

    refactored secured3d transaction

--- app/src/main/java/com/cyberspace/cyberpayapp/App.kt
+++ app/src/main/java/com/cyberspace/cyberpayapp/App.kt
@@ -9,8 +9,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        //CyberpaySdk.initialiseSdk("d5355204f9cf495f853c8f8d26ada19b", Mode.Debug)//3e1d7d40b9e043b694c7516148f23c3f
-        CyberpaySdk.initialiseSdk("3e1d7d40b9e043b694c7516148f23c3f", Mode.Live)
+        CyberpaySdk.initialiseSdk("d5355204f9cf495f853c8f8d26ada19b", Mode.Debug)
         CyberpaySdk.merchantLogo = resources.getDrawable(R.drawable.debit_card)
     }
 }
